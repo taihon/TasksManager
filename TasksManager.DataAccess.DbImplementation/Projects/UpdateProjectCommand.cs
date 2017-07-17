74371b80c5ed5d50b6060deba234d51966e250bf
taihon <taihon@users.noreply.github.com>
2017-07-17 23:56:00 +0700

    began working on update project command

---
+++ TasksManager.DataAccess.DbImplementation/Projects/UpdateProjectCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TasksManager.DataAccess.Projects;
+using TasksManager.Db;
+using TasksManager.Entities;
+using TasksManager.ViewModels.Projects;
+
+namespace TasksManager.DataAccess.DbImplementation.Projects
+{
+    public class UpdateProjectCommand:IUpdateProjectCommand
+    {
+        private TasksContext Context;
+        public UpdateProjectCommand(TasksContext tasksContext)
+        {
+            Context = tasksContext;
+        }
+        public async Task<ProjectResponse> ExecuteAsync(int projectId, UpdateProjectRequest request)
+        {
+            Project foundProject = await Context.Projects.FirstOrDefaultAsync(t => t.Id == projectId);
+            if (foundProject != null)
+            {
+                
+            }
+            return new ProjectResponse();
+        }
+    }
+}
