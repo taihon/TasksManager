72efb1aa2a7fbd92d746788806e8b0aeb1f3447a
taihon <taihon@users.noreply.github.com>
2017-07-23 12:32:30 +0700

    refactored CreateProjectCommand to use automapper

--- TasksManager.DataAccess.DbImplementation/Projects/CreateProjectCommand.cs
+++ TasksManager.DataAccess.DbImplementation/Projects/CreateProjectCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using AutoMapper;
 using TasksManager.DataAccess.Projects;
 using TasksManager.Db;
 using TasksManager.Entities;
@@ -8,27 +9,19 @@ namespace TasksManager.DataAccess.DbImplementation.Projects
 {
     public class CreateProjectCommand : ICreateProjectCommand
     {
-        private TasksContext Context { get; }
-        public CreateProjectCommand(TasksContext tasksContext)
+        private TasksContext _context;
+        private IMapper _mapper;
+        public CreateProjectCommand(TasksContext tasksContext, IMapper mapper)
         {
-            Context = tasksContext;
+            _context = tasksContext;
+            _mapper = mapper;
         }
         public async Task<ProjectResponse> ExecuteAsync(CreateProjectRequest request)
         {
-            var project = new Project
-            {
-                Name = request.Name,
-                Description = request.Description
-            };
-            await Context.Projects.AddAsync(project);
-            await Context.SaveChangesAsync();
-            return new ProjectResponse
-            {
-                Id = project.Id,
-                Name = project.Name,
-                Description = project.Description,
-                OpenTasksCount = 0
-            };
+            var project = _mapper.Map<CreateProjectRequest, Project>(request);
+            await _context.Projects.AddAsync(project);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<Project, ProjectResponse>(project);
         }
     }
 }
